1349d3e7ca11346e222c03521b8aff060ee13fad
Алексей Байрышевский <s3mods@yandex.ru>
2023-03-15 13:13:04 +0400

    Added AddEmployeeCommand

---
+++ src/EquipmentManagement.Application/AddEmployee/AddEmployeeCommand.cs
@@ -0,0 +1,18 @@
+﻿using EquipmentManagement.Application.Abstractions;
+
+namespace EquipmentManagement.Application.AddEmployee;
+
+public class AddEmployeeCommand : ICommand
+{
+    public string Firstname { get; }
+    public string Lastname { get; }
+    public string Patronymic { get; }
+    public AddEmployeeCommand(string firstname,
+                              string lastname,
+                              string patronymic)
+    {
+        Firstname = firstname;
+        Lastname = lastname;
+        Patronymic = patronymic;
+    }
+}
---
+++ src/EquipmentManagement.Application/AddEmployee/AddEmployeeCommandHandler.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using EquipmentManagement.Application.Abstractions;
+using EquipmentManagement.Domain.Abstractions.Repositories;
+using EquipmentManagement.Domain.Models;
+
+namespace EquipmentManagement.Application.AddEmployee;
+
+public class AddEmployeeCommandHandler : ICommandHandler<AddEmployeeCommand>
+{
+    private readonly IEmployeeRepository _employeeRepository;
+    private readonly IMapper _mapper;
+
+    public AddEmployeeCommandHandler(IEmployeeRepository employeeRepository, IMapper mapper)
+    {
+        _employeeRepository = employeeRepository;
+        _mapper = mapper;
+    }
+
+    public async Task Handle(AddEmployeeCommand request, CancellationToken cancellationToken)
+    {
+        var employee = _mapper.Map<Employee>(request);
+        await _employeeRepository.CreateAsync(employee, cancellationToken);
+    }
+}

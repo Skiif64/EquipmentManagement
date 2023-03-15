00b26f7401d196b271eb8a4d72f2ea6e709bcd30
Алексей Байрышевский <s3mods@yandex.ru>
2023-03-15 15:59:58 +0400

    Clearing EmployeeRepository

--- src/EquipmentManagement.DAL/Repositories/EmployeeRepository.cs
+++ src/EquipmentManagement.DAL/Repositories/EmployeeRepository.cs
@@ -8,69 +8,37 @@ namespace EquipmentManagement.DAL.Repositories;
 
 internal class EmployeeRepository : IEmployeeRepository
 {
-    private readonly ApplicationDbContext _context;
-    private bool _disposed;
+    private readonly ApplicationDbContext _context;      
 
     public EmployeeRepository(ApplicationDbContext context)
     {
-        _context = context;
+        _context = context;        
     }
     public async Task CreateAsync(Employee entity, CancellationToken cancellationToken)
-    {
-        if (_disposed)
-            throw new ObjectDisposedException(GetType().Name);
-
-        await _context.Employees.AddAsync(entity, cancellationToken);
+    {        
+        await _context.AddAsync(entity, cancellationToken);
         await _context.SaveChangesAsync();
     }
 
     public async Task DeleteAsync(Employee entity, CancellationToken cancellationToken)
-    {
-        if (_disposed)
-            throw new ObjectDisposedException(GetType().Name);
-
+    {        
         _context.Employees.Remove(entity);
         await _context.SaveChangesAsync();
-    }
-
-    public void Dispose()
-    {
-        if (_disposed)
-            return;
-        _disposed = true;
-        _context.Dispose();
-    }
-
-    public async ValueTask DisposeAsync()
-    {
-        if (_disposed)
-            return;
-        _disposed = true;
-        await _context.DisposeAsync();        
-    }
+    }    
 
     public async Task<IEnumerable<Employee>> GetAllAsync(CancellationToken cancellationToken)
-    {
-        if (_disposed)
-            throw new ObjectDisposedException(GetType().Name);
-
+    {        
         return _context.Employees;
     }    
 
     public async Task<Employee> GetByIdAsync(Guid id, CancellationToken cancellationToken)
-    {
-        if (_disposed)
-            throw new ObjectDisposedException(GetType().Name);
-
+    {  
         return await _context.Employees.FirstOrDefaultAsync(e => e.Id == id, cancellationToken)
             ?? throw new Exception();
     }
 
     public async Task UpdateAsync(Employee entity, CancellationToken cancellationToken)
     {
-        if (_disposed)
-            throw new ObjectDisposedException(GetType().Name);
-
         _context.Employees.Update(entity);
         await _context.SaveChangesAsync();
     }

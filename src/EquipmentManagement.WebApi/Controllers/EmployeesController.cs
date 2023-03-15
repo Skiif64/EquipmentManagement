b47256e1c54bceea9d8e39146d323d3b81a79631
Алексей Байрышевский <s3mods@yandex.ru>
2023-03-15 13:25:57 +0400

    Added EmployeesController

---
+++ src/EquipmentManagement.WebApi/Controllers/EmployeesController.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using EquipmentManagement.Application.Abstractions;
+using EquipmentManagement.Application.AddEmployee;
+using EquipmentManagement.Domain.Abstractions.Repositories;
+using EquipmentManagement.WebApi.Requests;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EquipmentManagement.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase   
+    {
+        private readonly ISender _sender;
+        private readonly IMapper _mapper;       
+        private readonly ILogger<EmployeesController>? _logger;
+        public EmployeesController(ISender sender, IMapper mapper, ILogger<EmployeesController>? logger = null)
+        {
+            _sender = sender;
+            _mapper = mapper;            
+            _logger = logger;
+        }
+        [HttpPost("add")]
+        public async Task<IActionResult> AddAsync(AddEmployeeRequest request, CancellationToken cancellationToken)
+        {
+            var command = _mapper.Map<AddEmployeeCommand>(request);
+            await _sender.Send(command, cancellationToken);
+            return Ok();
+        }
+    }
+}
---
+++ src/EquipmentManagement.WebApi/Requests/AddEmployeeRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace EquipmentManagement.WebApi.Requests;
+
+public class AddEmployeeRequest
+{
+    public string Firstname { get; set; }
+    public string Lastname { get; set; }
+    public string Patronymic { get; set; }
+}

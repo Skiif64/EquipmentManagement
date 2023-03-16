5b03ba5f859bf2f4edf7e6a1802f0d8ca3f3c087
Алексей Байрышевский <s3mods@yandex.ru>
2023-03-16 13:55:40 +0400

    Some changes

--- src/EquipmentManagement.WebApi/Program.cs
+++ src/EquipmentManagement.WebApi/Program.cs
@@ -1,6 +1,8 @@
 using EquipmentManagement.Application;
+using EquipmentManagement.Auth;
 using EquipmentManagement.DAL;
 using EquipmentManagement.WebApi;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,7 +16,7 @@ builder.Services.AddAutoMapper(cfg =>
 });
 builder.Services.AddApplication();
 builder.Services.AddDataAccess(builder.Configuration);
-
+builder.Services.AddApiKeyAuthorization();
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
@@ -24,5 +26,6 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseAuthorization();
+
 app.MapControllers();
 app.Run();

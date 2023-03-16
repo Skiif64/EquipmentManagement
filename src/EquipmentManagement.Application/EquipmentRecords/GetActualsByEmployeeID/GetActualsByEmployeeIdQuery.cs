120cc9da9e480451424722d7e373c46abb1f8f20
Алексей Байрышевский <s3mods@yandex.ru>
2023-03-16 10:12:33 +0400

    Fixed Equipment records handlers

--- src/EquipmentManagement.Application/EquipmentRecords/GetActualsByEmployeeID/GetActualsByEmployeeIdQuery.cs
+++ src/EquipmentManagement.Application/EquipmentRecords/GetActualsByEmployeeID/GetActualsByEmployeeIdQuery.cs
@@ -1,9 +1,9 @@
 ﻿using EquipmentManagement.Application.Abstractions;
 using EquipmentManagement.Domain.Models;
 
-namespace EquipmentManagement.Application.EquipmentStatuses.GetActualByEmployeeID;
+namespace EquipmentManagement.Application.EquipmentRecords.GetActualsByEmployeeID;
 
-public class GetActualsByEmployeeIdQuery : IQuery<IEnumerable<EquipmentRecord>>
+public class GetActualsByEmployeeIdQuery : IQuery<IEnumerable<EquipmentRecord>?>
 {
     public Guid EmployeeId { get; set; }
     public GetActualsByEmployeeIdQuery(Guid employeeId)
---
+++ src/EquipmentManagement.Application/EquipmentRecords/GetActualsByEmployeeID/GetActualsByEmployeeIdQueryHandler.cs
@@ -0,0 +1,26 @@
+﻿using EquipmentManagement.Application.Abstractions;
+using EquipmentManagement.Domain.Models;
+
+namespace EquipmentManagement.Application.EquipmentRecords.GetActualsByEmployeeID;
+
+public class GetActualsByEmployeeIdQueryHandler : IQueryHandler<GetActualsByEmployeeIdQuery, IEnumerable<EquipmentRecord>?>
+{
+    private readonly IApplicationDbContext _context;
+
+    public GetActualsByEmployeeIdQueryHandler(IApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public Task<IEnumerable<EquipmentRecord>?> Handle(GetActualsByEmployeeIdQuery request, CancellationToken cancellationToken)
+    {
+        var records = _context
+            .Set<EquipmentRecord>()
+            .Where(r => r.EmployeeId == request.EmployeeId)
+            .OrderBy(r => r.Modified)
+            .DistinctBy(r => r.EquipmentId)
+            ;
+
+        return Task.FromResult(records?.AsEnumerable());
+    }
+}

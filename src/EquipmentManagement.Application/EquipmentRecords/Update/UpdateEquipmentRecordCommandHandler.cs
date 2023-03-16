ffd109aa0c5efef8fd17fd672bce1599d0b2e154
Алексей Байрышевский <s3mods@yandex.ru>
2023-03-16 12:36:17 +0400

    Added UpdateEquipmentCommand&Handler

---
+++ src/EquipmentManagement.Application/EquipmentRecords/Update/UpdateEquipmentRecordCommandHandler.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using EquipmentManagement.Application.Abstractions;
+using EquipmentManagement.Application.Exceptions;
+using EquipmentManagement.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EquipmentManagement.Application.EquipmentRecords.Update;
+
+public class UpdateEquipmentRecordCommandHandler : ICommandHandler<UpdateEquipmentRecordCommand, EquipmentRecord>
+{
+    private readonly IApplicationDbContext _context;    
+
+    public UpdateEquipmentRecordCommandHandler(IApplicationDbContext context, IMapper mapper)
+    {
+        _context = context;
+        _mapper = mapper;
+    }
+
+    public async Task<EquipmentRecord> Handle(UpdateEquipmentRecordCommand request, CancellationToken cancellationToken)
+    {
+        var record = await _context
+            .Set<EquipmentRecord>()
+            .SingleOrDefaultAsync(r => r.Id == request.EquipmentRecordId, cancellationToken)
+            ?? throw new NotFoundException("EquipmentRecord")
+            ;
+
+        record.EmployeeId = request.EmployeeId;
+        if (request.StatusId is not null)
+            record.StatusId = (Guid)request.StatusId;
+        record.Modified= request.Modified;
+        _context.Set<EquipmentRecord>().Update(record);
+        await _context.SaveChangesAsync(cancellationToken);
+        return record;
+    }
+}

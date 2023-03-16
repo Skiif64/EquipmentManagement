b19e81a5b9d7c2532e0d9e6139a4b508c80b000b
Алексей Байрышевский <s3mods@yandex.ru>
2023-03-16 09:47:33 +0400

    Removed repositories

--- src/EquipmentManagement.Domain/Abstractions/Repositories/IStatusRepository.cs
+++
@@ -1,8 +0,0 @@
-﻿using EquipmentManagement.Domain.Models;
-
-namespace EquipmentManagement.Domain.Abstractions.Repositories;
-
-public interface IStatusRepository : ICrudRepository<Status>
-{
-    Task<Status> GetByEquipmentStatusIdAsync(Guid equipmentStatusId, CancellationToken cancellationToken);
-}

fb21e479b8c1565d534a82b26489bb710fe1bf52
Алексей Байрышевский <s3mods@yandex.ru>
2023-03-16 10:34:51 +0400

    Mark description as nullable in Equipment

--- src/EquipmentManagement.Domain/Models/Equipment.cs
+++ src/EquipmentManagement.Domain/Models/Equipment.cs
@@ -7,7 +7,7 @@ public class Equipment : BaseModel
 	public string Type { get; init; } = string.Empty;
 	public string Article { get; init; } = string.Empty;
     public string SerialNumber { get; init; } = string.Empty;
-    public string Description { get; init; } = string.Empty;
+    public string? Description { get; init; }
 
     protected Equipment() : base()
     {

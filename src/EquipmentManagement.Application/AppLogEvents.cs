72aee6a0cc573e5b4dddae48a60f2a666170c34a
Алексей Байрышевский <s3mods@yandex.ru>
2023-03-16 17:08:01 +0400

    Added AppLogEvents class

---
+++ src/EquipmentManagement.Application/AppLogEvents.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace EquipmentManagement.Application;
+
+public class AppLogEvents
+{
+    public static readonly EventId Create = new(1000, "Create");
+    public static readonly EventId Read = new(1000, "Read");
+    public static readonly EventId Update = new(1001, "Update");
+    public static readonly EventId Delete = new(1000, "Delete");
+}

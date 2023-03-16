c134f42f74c51ee4dfce6a0dea56477fada5084e
Алексей Байрышевский <s3mods@yandex.ru>
2023-03-16 09:47:15 +0400

    Added IApplicationDbContext interface

---
+++ src/EquipmentManagement.Application/Abstractions/IApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using EquipmentManagement.Domain.Models.Base;
+
+namespace EquipmentManagement.Application.Abstractions;
+
+public interface IApplicationDbContext
+{
+    IQueryable<TEntity> Set<TEntity>() where TEntity : BaseModel;
+
+    int SaveChanges();
+    Task<int> SaveChangesAsync();
+}

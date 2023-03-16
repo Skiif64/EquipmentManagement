2cf5e08c292f48116cfd0cd2c32c6030d5715633
Алексей Байрышевский <s3mods@yandex.ru>
2023-03-16 15:30:05 +0400

    Added Cookie Authentification

--- src/EquipmentManagement.Auth/DependencyInjection.cs
+++ src/EquipmentManagement.Auth/DependencyInjection.cs
@@ -1,15 +1,22 @@
 ﻿using EquipmentManagement.Auth.Abstractions;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace EquipmentManagement.Auth;
 
 public static class DependencyInjection
 {
-    public static IServiceCollection AddApiKeyAuthorization(this IServiceCollection services)
+    public static IServiceCollection AddAuth(this IServiceCollection services, IConfiguration configuration)
     {
+        var connectionString = configuration.GetConnectionString("Users");
         services.AddSingleton<IApiKeyValidator, ApiKeyValidator>();
         services.AddSingleton<ApiKeyAuthorizationFilter>();
-
+        services.AddDbContext<UserStoreDbContext>(opt => opt.UseNpgsql(connectionString));
+        services.AddIdentity<IdentityUser, IdentityRole>()
+            .AddEntityFrameworkStores<UserStoreDbContext>();
+            
         return services;
     }
 }
---
+++ src/EquipmentManagement.Auth/UserStoreDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace EquipmentManagement.Auth;
+
+internal class UserStoreDbContext : IdentityDbContext
+{
+	public UserStoreDbContext(DbContextOptions options) : base(options)
+	{
+		Database.Migrate();
+	}
+}

55cf16978307b4d094a7943e6d92c974fdb794dd
Алексей Байрышевский <s3mods@yandex.ru>
2023-03-16 11:15:13 +0400

    Added AddEquipmentRecordCommand&handler

---
+++ src/EquipmentManagement.Application/EquipmentRecords/Add/AddEquipmentRecordCommand.cs
@@ -0,0 +1,16 @@
+﻿using EquipmentManagement.Application.Abstractions;
+
+namespace EquipmentManagement.Application.EquipmentRecords.Add;
+
+public class AddEquipmentRecordCommand : ICommand<Guid>
+{
+    public Guid? EmployeeId { get; init; }
+    public Guid EquipmentId { get; set; }
+    public Guid StatusId { get; init; }
+    public DateTimeOffset Modified { get; }
+
+    public AddEquipmentRecordCommand()
+    {
+        Modified = DateTimeOffset.Now;
+    }
+}
---
+++ src/EquipmentManagement.Application/EquipmentRecords/Add/AddEquipmentRecordCommandHandler.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using EquipmentManagement.Application.Abstractions;
+using EquipmentManagement.Domain.Models;
+
+namespace EquipmentManagement.Application.EquipmentRecords.Add;
+
+public class AddEquipmentRecordCommandHandler : ICommandHandler<AddEquipmentRecordCommand, Guid>
+{
+    private readonly IApplicationDbContext _context;
+    private readonly IMapper _mapper;
+
+    public AddEquipmentRecordCommandHandler(IApplicationDbContext context, IMapper mapper)
+    {
+        _context = context;
+        _mapper = mapper;
+    }
+    public async Task<Guid> Handle(AddEquipmentRecordCommand request, CancellationToken cancellationToken)
+    {
+        var record = _mapper.Map<EquipmentRecord>(request);
+        await _context.Set<EquipmentRecord>().AddAsync(record);
+        return record.Id;
+    }
+}

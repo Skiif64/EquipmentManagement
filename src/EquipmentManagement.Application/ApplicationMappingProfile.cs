64b57b6408efe0616c5c1da725d8dc81c24ce99c
Алексей Байрышевский <s3mods@yandex.ru>
2023-03-16 10:49:04 +0400

    Added ApplicationMappingProfile

---
+++ src/EquipmentManagement.Application/ApplicationMappingProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using EquipmentManagement.Application.Employees.Add;
+using EquipmentManagement.Application.Equipments.Add;
+using EquipmentManagement.Domain.Models;
+
+namespace EquipmentManagement.Application;
+
+public class ApplicationMappingProfile : Profile
+{
+    public ApplicationMappingProfile()
+    {
+        CreateMap<AddEmployeeCommand, Employee>();
+        CreateMap<AddEquipmentCommand, Equipment>();
+        
+    }
+}

e0aabc240af8576f084f6b2e7f1b8f9b1a2f51ad
Алексей Байрышевский <s3mods@yandex.ru>
2023-03-17 15:07:15 +0400

    Registered authorizationcore

--- ui/EquipmentManagement.UI/Program.cs
+++ ui/EquipmentManagement.UI/Program.cs
@@ -8,7 +8,7 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
+builder.Services.AddAuthorizationCore();
 var app = builder.Build();
 
 await app.RunAsync();

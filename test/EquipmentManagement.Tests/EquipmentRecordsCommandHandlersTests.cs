5fb92d5b162b4e0436f5a8318a78f7915b2d1c99
Алексей Байрышевский <s3mods@yandex.ru>
2023-03-17 10:47:42 +0400

    Added first test

---
+++ test/EquipmentManagement.Tests/EquipmentRecordsCommandHandlersTests.cs
@@ -0,0 +1,74 @@
+﻿using AutoFixture.NUnit3;
+using EquipmentManagement.Application.Abstractions;
+using EquipmentManagement.Application.EquipmentRecords.GetActualsByEmployeeID;
+using EquipmentManagement.Domain.Models;
+using EquipmentManagement.Tests.Fixtures;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+
+namespace EquipmentManagement.Tests;
+
+internal class EquipmentRecordsCommandHandlersTests
+{
+    private readonly Mock<IApplicationDbContext> _contextMock;
+    private readonly Mock<DbSet<EquipmentRecord>> _equipmentSetMock;
+    public EquipmentRecordsCommandHandlersTests()
+    {
+        _contextMock = new Mock<IApplicationDbContext>();
+        _equipmentSetMock = new Mock<DbSet<EquipmentRecord>>();
+    }
+
+    [SetUp]
+    public void ClearMocks()
+    {
+        _contextMock.Reset();
+        _equipmentSetMock.Reset();
+    }
+
+    [Test, AutoData]
+    public async Task WhenGetActualByEmployeeId__ShouldReturn3Records(Guid employeeId1, Guid employeeId2, Guid employeeId3)
+    {
+        var sut = new GetActualsByEmployeeIdQueryHandler(_contextMock.Object);
+        var records = new[]
+        {
+            EquipmentRecordsFixtures.Create(employeeId1),
+            EquipmentRecordsFixtures.Create(employeeId1),
+            EquipmentRecordsFixtures.Create(employeeId1),
+            EquipmentRecordsFixtures.Create(employeeId2),
+            EquipmentRecordsFixtures.Create(employeeId2),
+            EquipmentRecordsFixtures.Create(employeeId2),
+            EquipmentRecordsFixtures.Create(employeeId3),
+            EquipmentRecordsFixtures.Create(employeeId3),
+            EquipmentRecordsFixtures.Create(employeeId3)
+        }.AsQueryable();
+        SetupSet(_equipmentSetMock.As<IQueryable<EquipmentRecord>>(), records);
+
+        _contextMock.Setup(x => x.Set<EquipmentRecord>())
+            .Returns(_equipmentSetMock.Object);
+        var request = new GetActualsByEmployeeIdQuery(employeeId1);
+
+
+        var actual = await sut.Handle(request, default);
+
+        Assert.That(actual, Is.Not.Null);
+        CollectionAssert.AllItemsAreNotNull(actual);
+        Assert.That(actual.Count(), Is.EqualTo(3));
+
+    }
+
+    private void SetupSet<T>(Mock<IQueryable<T>> set, IQueryable<T> data)
+    {
+        set
+            .Setup(x => x.Provider)
+            .Returns(data.Provider);
+        set
+            .Setup(x => x.Expression)
+            .Returns(data.Expression);
+        set
+            .Setup(x => x.ElementType)
+            .Returns(data.ElementType);
+        set
+            .Setup(x => x.GetEnumerator())
+            .Returns(data.GetEnumerator());
+    }
+}
---
+++ test/EquipmentManagement.Tests/Fixtures/EquipmentRecordsFixtures.cs
@@ -0,0 +1,45 @@
+﻿using AutoFixture;
+using EquipmentManagement.Domain.Models;
+
+namespace EquipmentManagement.Tests.Fixtures;
+
+public static class EquipmentRecordsFixtures
+{
+    private class TestEquipmentRecord : EquipmentRecord
+    {
+        public TestEquipmentRecord() : base()
+        { }
+    }
+    private static readonly Fixture _fixture = new();
+    public static EquipmentRecord Create()
+        => _fixture.Build<EquipmentRecord>()        
+        .With(x => x.EmployeeId, _fixture.Create<Guid>())
+        .With(x => x.EquipmentId, _fixture.Create<Guid>())
+        .With(x => x.StatusId, _fixture.Create<Guid>())
+        .With(x => x.Modified, _fixture.Create<DateTimeOffset>())
+        .Create<TestEquipmentRecord>();
+
+
+    static EquipmentRecordsFixtures()
+    {
+        _fixture.Register<EquipmentRecord>(() => new TestEquipmentRecord());
+    }
+
+    public static EquipmentRecord Create(Guid employeeId)
+    {
+        var record = Create();
+        record.EmployeeId = employeeId;
+        return record;
+    }
+    public static EquipmentRecord Create(Guid employeeId, Guid equipmentId)
+    {
+        var record = Create();
+        record.EmployeeId = employeeId;
+        record.EquipmentId = equipmentId;
+        return record;
+    }
+
+
+    public static IEnumerable<EquipmentRecord> CreateMany(int count) 
+        => _fixture.CreateMany<EquipmentRecord>(count);
+}

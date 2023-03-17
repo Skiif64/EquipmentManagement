cd528485c56a5bfcf220994e40358a6a71cc0884
Алексей Байрышевский <s3mods@yandex.ru>
2023-03-17 15:33:25 +0400

    Added custom authentification state provider

--- ui/EquipmentManagement.UI/Program.cs
+++ ui/EquipmentManagement.UI/Program.cs
@@ -1,5 +1,6 @@
 using EquipmentManagement.UI;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -8,6 +9,7 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthentificationStateProvider>();
 builder.Services.AddAuthorizationCore();
 var app = builder.Build();
 

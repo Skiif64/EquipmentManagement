8446d9763950ce107ce5078e56b138a55a75cf8a
Алексей Байрышевский <s3mods@yandex.ru>
2023-03-20 20:28:15 +0300

    Added application user

---
+++ src/EquipmentManagement.Application/Models/ApplicationUser.cs
@@ -0,0 +1,10 @@
+﻿using EquipmentManagement.Domain.Models.Base;
+
+namespace EquipmentManagement.Application.Models;
+
+public class ApplicationUser : BaseModel
+{
+    public string Login { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+    public string Role { get; set; } = string.Empty;
+}

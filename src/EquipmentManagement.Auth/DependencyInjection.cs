2c6d1081ab654189ea94e9bcdb43f684876f9843
Алексей Байрышевский <s3mods@yandex.ru>
2023-03-20 14:52:04 +0400

    Added base for jwt

--- src/EquipmentManagement.Auth/DependencyInjection.cs
+++ src/EquipmentManagement.Auth/DependencyInjection.cs
@@ -1,7 +1,10 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 namespace EquipmentManagement.Auth;
 
@@ -12,13 +15,23 @@ public static class DependencyInjection
         var connectionString = configuration.GetConnectionString("Users");
         
         services.AddDbContext<UserStoreDbContext>(opt => opt.UseNpgsql(connectionString));
-        services.AddIdentity<IdentityUser, IdentityRole>()
-            .AddEntityFrameworkStores<UserStoreDbContext>();
-        var provider = services.BuildServiceProvider();
+        services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(opt =>
+            {
+                opt.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateAudience = false,
+                    ValidateIssuer = false,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("super-secret-key"))
+                };
+            });
+        //var provider = services.BuildServiceProvider();
 
-        var roleManager = provider.GetRequiredService<RoleManager<IdentityRole>>();
-        var userManager = provider.GetRequiredService<UserManager<IdentityUser>>();
-        UserInitializer.Initialize(roleManager, userManager, configuration).Wait();
+        //var roleManager = provider.GetRequiredService<RoleManager<IdentityRole>>();
+        //var userManager = provider.GetRequiredService<UserManager<IdentityUser>>();
+        //UserInitializer.Initialize(roleManager, userManager, configuration).Wait();
         return services;
     }
 }
---
+++ src/EquipmentManagement.Auth/JwtAuthentificationProvider.cs
@@ -0,0 +1,46 @@
+﻿using EquipmentManagement.Auth.Models;
+using EquipmentManagement.Domain.Models;
+using Microsoft.IdentityModel.JsonWebTokens;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace EquipmentManagement.Auth;
+
+internal class JwtAuthentificationProvider
+{
+    private readonly UserStoreDbContext _userStoreDbContext;
+
+    public JwtAuthentificationProvider(UserStoreDbContext userStoreDbContext)
+    {
+        _userStoreDbContext = userStoreDbContext;
+    }
+
+    private string CreateJWT(IdentityUser user)
+    {
+        var secretKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("super-secret-key"));
+        var credentials = new SigningCredentials(secretKey, SecurityAlgorithms.HmacSha256);
+
+        var claims = new[]
+        {
+            new Claim(ClaimTypes.Name, user.Username),
+            new Claim(ClaimTypes.Role, user.Role)
+        };
+
+        var token = new JwtSecurityToken(
+            audience: "audience",
+            issuer: "issuer",
+            claims: claims,
+            expires: DateTime.UtcNow.AddHours(1),
+            signingCredentials: credentials
+            );
+        var tokenHandler = new JwtSecurityTokenHandler();
+        return tokenHandler.WriteToken(token);        
+    }
+
+    public async Task<bool> SigninAsync(IdentityUser user, string password, CancellationToken cancellationToken)
+    {
+
+    }
+}

915449e7dd6522557d3cfc9fa2f8e6edfa4ad7fa
Алексей Байрышевский <s3mods@yandex.ru>
2023-03-21 16:23:22 +0400

    Added new migration

--- src/EquipmentManagement.DAL/Migrations/20230321122302_UpdatedDomain.Designer.cs
+++ src/EquipmentManagement.DAL/Migrations/20230321122302_UpdatedDomain.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using EquipmentManagement.DAL;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,10 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace EquipmentManagement.DAL.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230321122302_UpdatedDomain")]
+    partial class UpdatedDomain
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/EquipmentManagement.DAL/Migrations/20230321122302_UpdatedDomain.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EquipmentManagement.DAL.Migrations
+{
+    public partial class UpdatedDomain : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_EquipmentRecords_EmployeeId",
+                table: "EquipmentRecords",
+                column: "EmployeeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EquipmentRecords_EquipmentId",
+                table: "EquipmentRecords",
+                column: "EquipmentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EquipmentRecords_StatusId",
+                table: "EquipmentRecords",
+                column: "StatusId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_EquipmentRecords_Employees_EmployeeId",
+                table: "EquipmentRecords",
+                column: "EmployeeId",
+                principalTable: "Employees",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_EquipmentRecords_Equipments_EquipmentId",
+                table: "EquipmentRecords",
+                column: "EquipmentId",
+                principalTable: "Equipments",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_EquipmentRecords_Statuses_StatusId",
+                table: "EquipmentRecords",
+                column: "StatusId",
+                principalTable: "Statuses",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_EquipmentRecords_Employees_EmployeeId",
+                table: "EquipmentRecords");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_EquipmentRecords_Equipments_EquipmentId",
+                table: "EquipmentRecords");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_EquipmentRecords_Statuses_StatusId",
+                table: "EquipmentRecords");
+
+            migrationBuilder.DropIndex(
+                name: "IX_EquipmentRecords_EmployeeId",
+                table: "EquipmentRecords");
+
+            migrationBuilder.DropIndex(
+                name: "IX_EquipmentRecords_EquipmentId",
+                table: "EquipmentRecords");
+
+            migrationBuilder.DropIndex(
+                name: "IX_EquipmentRecords_StatusId",
+                table: "EquipmentRecords");
+        }
+    }
+}

d516ff4ce47fe89e923c01e80c8f7a3e84d63cf8
Алексей Байрышевский <s3mods@yandex.ru>
2023-03-21 16:44:22 +0400

    Added GetStatusByIdQuery&handler

---
+++ src/EquipmentManagement.Application/Statuses/Get/GetStatusByIdQuery.cs
@@ -0,0 +1,13 @@
+﻿using EquipmentManagement.Application.Abstractions;
+using EquipmentManagement.Domain.Models;
+
+namespace EquipmentManagement.Application.Statuses.Get;
+
+public class GetStatusByIdQuery : IQuery<Status>
+{
+    public Guid StatusId { get; set; }
+    public GetStatusByIdQuery(Guid id)
+    {
+        StatusId = id;
+    }
+}

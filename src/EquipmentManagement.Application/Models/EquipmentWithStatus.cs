68f8c3fb9fdb42a8e50a15b799a50bd84cfbb48c
Алексей Байрышевский <s3mods@yandex.ru>
2023-03-21 15:20:07 +0400

    Added EquipmentWithStatus model

---
+++ src/EquipmentManagement.Application/Models/EquipmentWithStatus.cs
@@ -0,0 +1,9 @@
+﻿using EquipmentManagement.Domain.Models;
+
+namespace EquipmentManagement.Application.Models;
+
+public class EquipmentWithStatus
+{
+    public Equipment Equipment { get; set; }
+    public Status? Status { get; set; }    
+}

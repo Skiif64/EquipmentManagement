a928b3cc064f0583c88b2935e52d1ba47cde7baa
Алексей Байрышевский <s3mods@yandex.ru>
2023-03-21 10:51:35 +0400

    Added employee tesponse mapping profile

--- src/EquipmentManagement.WebApi/WebApiMappingProfile.cs
+++ src/EquipmentManagement.WebApi/WebApiMappingProfile.cs
@@ -1,9 +1,11 @@
 ﻿using AutoMapper;
 using EquimentManagement.Contracts.Requests;
+using EquimentManagement.Contracts.Responses;
 using EquipmentManagement.Application.Employees.Add;
 using EquipmentManagement.Application.EquipmentRecords.Add;
 using EquipmentManagement.Application.EquipmentRecords.Update;
 using EquipmentManagement.Application.Equipments.Add;
+using EquipmentManagement.Domain.Models;
 
 namespace EquipmentManagement.WebApi;
 
@@ -15,5 +17,7 @@ public class WebApiMappingProfile : Profile
         CreateMap<AddEquipmentRequest, AddEquipmentCommand>();
         CreateMap<AddEquipmentRecordRequest, AddEquipmentRecordCommand>();
         CreateMap<UpdateEquipmentRecordRequest, UpdateEquipmentRecordCommand>();
+
+        CreateMap<Employee, EmployeeResponse>();
     }
 }

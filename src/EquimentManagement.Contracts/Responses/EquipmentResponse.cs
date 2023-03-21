f94e120ec94ffd6b7cbf0d5881be4eedb91d0ed8
Алексей Байрышевский <s3mods@yandex.ru>
2023-03-21 14:21:29 +0400

    Added equipment response

---
+++ src/EquimentManagement.Contracts/Responses/EquipmentResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace EquimentManagement.Contracts.Responses;
+
+public class EquipmentResponse
+{
+    public Guid Id { get; set; }
+    public string Type { get; init; } = string.Empty;
+    public string Article { get; init; } = string.Empty;
+    public string SerialNumber { get; init; } = string.Empty;
+    public string? Description { get; init; }
+}

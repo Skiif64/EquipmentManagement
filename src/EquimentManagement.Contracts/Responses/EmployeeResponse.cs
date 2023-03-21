efc3156496b72686cd85c684dba349b798e3d5a9
Алексей Байрышевский <s3mods@yandex.ru>
2023-03-21 10:51:17 +0400

    Added employeeResponse

---
+++ src/EquimentManagement.Contracts/Responses/EmployeeResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace EquimentManagement.Contracts.Responses;
+
+public class EmployeeResponse
+{
+    public Guid Id { get; set; }
+    public string Firstname { get; init; } = string.Empty;
+    public string Lastname { get; init; } = string.Empty;
+    public string? Patronymic { get; init; }
+}

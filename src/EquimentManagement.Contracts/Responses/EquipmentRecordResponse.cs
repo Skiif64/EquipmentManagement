a1633bc467451af9dc6acc992f4f9549fcce6d47
Алексей Байрышевский <s3mods@yandex.ru>
2023-03-22 10:05:04 +0400

    Added EquipmentRecordResponse

---
+++ src/EquimentManagement.Contracts/Responses/EquipmentRecordResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace EquimentManagement.Contracts.Responses;
+
+public class EquipmentRecordResponse
+{
+    public Guid Id { get; set; }
+    public virtual Guid? EmployeeId { get; set; }
+    public virtual Guid EquipmentId { get; set; }
+    public virtual Guid StatusId { get; set; }
+    public DateTimeOffset Modified { get; set; }
+}

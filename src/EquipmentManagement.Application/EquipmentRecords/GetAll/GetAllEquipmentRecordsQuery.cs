6e2effccf99973bdc5d3aab899dfa3a5a214d1ef
Алексей Байрышевский <s3mods@yandex.ru>
2023-03-22 13:58:08 +0400

    Added GetAllEquipmentRecordsQuery&handler

---
+++ src/EquipmentManagement.Application/EquipmentRecords/GetAll/GetAllEquipmentRecordsQuery.cs
@@ -0,0 +1,8 @@
+﻿using EquipmentManagement.Application.Abstractions;
+using EquipmentManagement.Domain.Models;
+
+namespace EquipmentManagement.Application.EquipmentRecords.GetAll;
+
+public class GetAllEquipmentRecordsQuery : IQuery<IEnumerable<EquipmentRecord>>
+{
+}

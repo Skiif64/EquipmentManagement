78480c3fd9af052ec8c7ccf22adaba23acc349ea
Алексей Байрышевский <s3mods@yandex.ru>
2023-03-22 16:45:28 +0400

    Added equipmentWithStatusResponse

---
+++ src/EquimentManagement.Contracts/Responses/EquipmentWithStatusResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace EquimentManagement.Contracts.Responses;
+
+public class EquipmentWithStatusResponse : EquipmentResponse
+{
+    public Guid? AssignedTo { get; set; }
+    public Guid? StatusId { get; set; }
+    public string? StatusTitle { get; set; }
+}

0b4ac732e36d9331e7eb2db3c83c0a2c8cc00bd9
Алексей Байрышевский <s3mods@yandex.ru>
2023-03-23 16:05:22 +0400

    Added refreshToken method to authHandler

--- ui/EquipmentManagement.UI/Authentification/AuthenticationHttpMessageHandler.cs
+++ ui/EquipmentManagement.UI/Authentification/AuthenticationHttpMessageHandler.cs
@@ -1,4 +1,5 @@
 ﻿using EquipmentManagement.UI.Abstractions;
+using System.Net;
 using System.Net.Http.Headers;
 
 namespace EquipmentManagement.UI.Authentification;
@@ -17,6 +18,13 @@ public class AuthenticationHttpMessageHandler : DelegatingHandler
         var token = await _tokenStorage.GetAccessTokenAsync(cancellationToken);
         request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token);
         var response = await base.SendAsync(request, cancellationToken);
+        if (response.StatusCode == HttpStatusCode.Unauthorized)
+            await RefreshTokens(cancellationToken);
         return response;
+    }
+
+    private async Task RefreshTokens(CancellationToken cancellationToken)
+    {
+        
     }
 }

c0a48256766906c18f664c1b1bbc57f954dcb96f
Алексей Байрышевский <s3mods@yandex.ru>
2023-03-24 15:11:32 +0400

    Add NotDefaultValueAttribute

---
+++ src/EquimentManagement.Contracts/NotDefaultValueAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace EquimentManagement.Contracts;
+public class NotDefaultValueAttribute : ValidationAttribute
+{
+    public override string FormatErrorMessage(string name)
+    {
+        return $"Field {name} is null or default value";
+    }
+
+    public override bool IsValid(object? value)
+    {
+        return value != default;
+    }
+}

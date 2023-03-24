ce14f4e71457e3c6563c20d2fb7ca020067246a7
Алексей Байрышевский <s3mods@yandex.ru>
2023-03-24 10:17:15 +0400

    Added Jwt Options Setups

---
+++ src/EquipmentManagement.WebApi/OptionSetups/JwtBearerOptionsSetup.cs
@@ -0,0 +1,29 @@
+﻿using EquipmentManagement.Auth;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+
+namespace EquipmentManagement.WebApi.OptionSetups;
+
+public class JwtBearerOptionsSetup : IConfigureOptions<JwtBearerOptions>
+{
+    private readonly JwtTokenOptions _options;
+
+    public JwtBearerOptionsSetup(IOptions<JwtTokenOptions> jwtOptions)
+    {
+        _options = jwtOptions.Value;
+    }
+
+    public void Configure(JwtBearerOptions options)
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateAudience = true,
+            ValidAudience = _options.Audience,
+            ValidateIssuer = true,
+            ValidIssuer = _options.Issuer,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true
+        };
+    }
+}
---
+++ src/EquipmentManagement.WebApi/OptionSetups/JwtTokenOptionsSetup.cs
@@ -0,0 +1,20 @@
+﻿using EquipmentManagement.Auth;
+using Microsoft.Extensions.Options;
+
+namespace EquipmentManagement.WebApi.OptionSetups;
+
+public class JwtTokenOptionsSetup : IConfigureOptions<JwtTokenOptions>
+{
+    private const string SectionName = "Jwt";
+    private readonly IConfiguration _configuration;
+
+    public JwtTokenOptionsSetup(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    public void Configure(JwtTokenOptions options)
+    {
+        _configuration.GetRequiredSection(SectionName).Bind(options);
+    }
+}

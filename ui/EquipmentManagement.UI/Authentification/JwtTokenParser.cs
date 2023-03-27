b5ccb95b52282330ed81f85bd38759943d61d861
Алексей Байрышевский <s3mods@yandex.ru>
2023-03-27 12:22:35 +0400

    Added jwt token parser class

---
+++ ui/EquipmentManagement.UI/Authentification/JwtTokenParser.cs
@@ -0,0 +1,13 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+
+namespace EquipmentManagement.UI.Authentification;
+
+public class JwtTokenParser
+{
+    public static JwtSecurityToken Parse(string token)
+    {
+        var handler = new JwtSecurityTokenHandler();
+        var parsedToken = handler.ReadJwtToken(token);
+        return parsedToken;
+    }
+}

c41995cd403482e9786e013e16b951c1b1c0bdab
Алексей Байрышевский <s3mods@yandex.ru>
2023-03-28 10:21:19 +0400

    Removed update token logic

--- ui/EquipmentManagement.UI/Authentification/AuthenticationHttpMessageHandler.cs
+++ ui/EquipmentManagement.UI/Authentification/AuthenticationHttpMessageHandler.cs
@@ -7,36 +7,20 @@ namespace EquipmentManagement.UI.Authentification;
 public class AuthenticationHttpMessageHandler : DelegatingHandler
 {
     private readonly ITokenStorage _tokenStorage;
-    private readonly IServiceProvider _serviceProvider;
+   
 
-    public AuthenticationHttpMessageHandler(ITokenStorage tokenStorage, IServiceProvider serviceProvider)
+    public AuthenticationHttpMessageHandler(ITokenStorage tokenStorage)
     {
-        _tokenStorage = tokenStorage;
-        _serviceProvider = serviceProvider;
+        _tokenStorage = tokenStorage;       
     }
 
     protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
     {
         var token = await _tokenStorage.GetAccessTokenAsync(cancellationToken);
-        //if (TokenExpire(token))
-            //await RefreshTokenAsync(cancellationToken);
+        
         request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token);
         var response = await base.SendAsync(request, cancellationToken);
         
         return response;
-    }    
-
-    private bool TokenExpire(string? token)
-    {
-        if (token is null)
-            return true;
-        var parsedToken = JwtTokenParser.Parse(token);
-        return parsedToken.ValidTo > DateTime.UtcNow;        
-    }
-
-    private async Task RefreshTokenAsync(CancellationToken cancellationToken)
-    {
-        var refresher = _serviceProvider.GetRequiredService<IJwtTokenRefresher>();        
-        await refresher.RefreshAccessToken(cancellationToken);
-    }
+    } 
 }

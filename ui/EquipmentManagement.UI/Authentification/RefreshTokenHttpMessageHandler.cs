10fd6ecd11f28f00a3938df1e5662e43adefb122
Алексей Байрышевский <s3mods@yandex.ru>
2023-03-28 10:21:34 +0400

    Added refreshTokenHttpMessageHandler

---
+++ ui/EquipmentManagement.UI/Authentification/RefreshTokenHttpMessageHandler.cs
@@ -0,0 +1,20 @@
+﻿using EquipmentManagement.UI.Utils;
+
+namespace EquipmentManagement.UI.Authentification;
+
+public class RefreshTokenHttpMessageHandler : DelegatingHandler
+{
+    protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+    {
+        var response = base.SendAsync(request, cancellationToken);
+        return response;
+    }
+
+    private bool TokenExpire(string? token)
+    {
+        if (token is null)
+            return true;
+        var parsedToken = JwtTokenParser.Parse(token);
+        return parsedToken.ValidTo > DateTime.UtcNow;
+    }
+}

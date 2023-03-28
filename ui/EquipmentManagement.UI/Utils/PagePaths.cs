10c7db5003565d0d91ecf7d4942c88600a774456
Алексей Байрышевский <s3mods@yandex.ru>
2023-03-28 13:57:14 +0400

    Added PagePath helper

---
+++ ui/EquipmentManagement.UI/Utils/PagePaths.cs
@@ -0,0 +1,28 @@
+﻿namespace EquipmentManagement.UI.Utils;
+
+public static class PagePaths
+{
+    public const string Home = "/";
+    public static class Auth
+    {
+        public const string Login = "/auth/login";
+        public const string Logout = "/auth/logout";
+        public const string Register = "/auth/register";
+    }
+    public static class Employee
+    {
+        public const string List = "/employee";
+        public const string Add = "/employee/add";
+    }
+    public static class Equipment
+    {
+        public const string List = "/equipment";
+        public const string Add = "/equipment/add";
+        public const string EmployeeEquipment = "/equipment/employee";
+    }
+    public static class EquipmentRecord
+    {
+        public const string History = "/record/history";
+        public const string Add = "/record/add";       
+    }
+}

07d1a40241495b81093abdeb400bb362fe89d5af
Алексей Байрышевский <s3mods@yandex.ru>
2023-03-29 14:24:16 +0400

    AddStatusCommandHandler now return status instead of guid

--- src/EquipmentManagement.Application/Statuses/Add/AddStatusCommand.cs
+++ src/EquipmentManagement.Application/Statuses/Add/AddStatusCommand.cs
@@ -1,8 +1,9 @@
 ﻿using EquipmentManagement.Application.Abstractions;
+using EquipmentManagement.Domain.Models;
 
 namespace EquipmentManagement.Application.Statuses.Add;
 
-public class AddStatusCommand : ICommand<Guid>
+public class AddStatusCommand : ICommand<Status>
 {
     public string Title { get; set; } = string.Empty;
     public string? Description { get; set; }
--- src/EquipmentManagement.Application/Statuses/Add/AddStatusCommandHandler.cs
+++ src/EquipmentManagement.Application/Statuses/Add/AddStatusCommandHandler.cs
@@ -4,7 +4,7 @@ using EquipmentManagement.Domain.Models;
 
 namespace EquipmentManagement.Application.Statuses.Add;
 
-public class AddStatusCommandHandler : ICommandHandler<AddStatusCommand, Guid>
+public class AddStatusCommandHandler : ICommandHandler<AddStatusCommand, Status>
 {
     private readonly IApplicationDbContext _context;
     private readonly IMapper _mapper;
@@ -15,11 +15,11 @@ public class AddStatusCommandHandler : ICommandHandler<AddStatusCommand, Guid>
         _mapper = mapper;
     }
 
-    public async Task<Guid> Handle(AddStatusCommand request, CancellationToken cancellationToken)
+    public async Task<Status> Handle(AddStatusCommand request, CancellationToken cancellationToken)
     {
         var status = _mapper.Map<Status>(request);
         await _context.Set<Status>().AddAsync(status, cancellationToken);
         await _context.SaveChangesAsync(cancellationToken);
-        return status.Id;
+        return status;
     }
 }

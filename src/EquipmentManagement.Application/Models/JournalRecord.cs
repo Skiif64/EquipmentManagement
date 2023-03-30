963f1cca71debe064b65ca8d2613dd4c6a213575
Алексей Байрышевский <s3mods@yandex.ru>
2023-03-30 13:38:21 +0400

    Changed journal record

--- src/EquipmentManagement.Application/Models/JournalRecord.cs
+++ src/EquipmentManagement.Application/Models/JournalRecord.cs
@@ -3,8 +3,8 @@
 namespace EquipmentManagement.Application.Models;
 public class JournalRecord : BaseModel
 {
-    public Guid ApplicationUserId { get; set; }
-    public ApplicationUser ApplicationUser { get; set; } = null!;
+    public Guid? ApplicationUserId { get; set; }
+    public ApplicationUser? ApplicationUser { get; set; }
     public string Message { get; set; } = string.Empty;
     public string EventName { get; set; } = string.Empty;
     protected JournalRecord() : base()

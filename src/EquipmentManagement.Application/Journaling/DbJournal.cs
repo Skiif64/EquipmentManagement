c17399d06bec40977f440b269ef3ce0991885758
Алексей Байрышевский <s3mods@yandex.ru>
2023-03-30 13:36:52 +0400

    Added DbJournal

---
+++ src/EquipmentManagement.Application/Journaling/DbJournal.cs
@@ -0,0 +1,21 @@
+﻿using EquipmentManagement.Application.Abstractions;
+using EquipmentManagement.Application.Models;
+
+namespace EquipmentManagement.Application.Journaling;
+public class DbJournal : IJournal
+{
+    private readonly IApplicationDbContext _context;
+
+    public DbJournal(IApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task WriteAsync(JournalRecord record, CancellationToken cancellationToken)
+    {
+        await _context
+            .Set<JournalRecord>()
+            .AddAsync(record, cancellationToken);
+        await _context.SaveChangesAsync(cancellationToken);
+    }
+}

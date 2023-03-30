fb3b8c7aa8496bcacc285e2b63a2535df38d5b10
Алексей Байрышевский <s3mods@yandex.ru>
2023-03-30 16:00:45 +0400

    Added Migration

---
+++ src/EquipmentManagement.DAL/Migrations/20230330120035_AddedRecordCreatedAtTime.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EquipmentManagement.DAL.Migrations
+{
+    public partial class AddedRecordCreatedAtTime : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "DateCreated",
+                table: "Journal",
+                type: "timestamp with time zone",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DateCreated",
+                table: "Journal");
+        }
+    }
+}

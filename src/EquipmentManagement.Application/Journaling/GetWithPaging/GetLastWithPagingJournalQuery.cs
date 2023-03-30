b29ab0bdeb259eeb3cd4cf9bf3de3a0cd1e57b7c
Алексей Байрышевский <s3mods@yandex.ru>
2023-03-30 16:09:35 +0400

    Added GetLastJournalWithPagingQuery&Handler

---
+++ src/EquipmentManagement.Application/Journaling/GetWithPaging/GetLastWithPagingJournalQuery.cs
@@ -0,0 +1,15 @@
+﻿using EquipmentManagement.Application.Abstractions;
+using EquipmentManagement.Application.Models;
+
+namespace EquipmentManagement.Application.Journaling.GetWithPaging;
+public class GetLastWithPagingJournalQuery : IQuery<IEnumerable<JournalRecord>>
+{
+    public int Count { get; set; }
+    public int Offset { get; set; }
+
+    public GetLastWithPagingJournalQuery(int count, int offset)
+    {
+        Count = count;
+        Offset = offset;
+    }
+}

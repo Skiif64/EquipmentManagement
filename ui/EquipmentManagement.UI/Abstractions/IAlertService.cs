410a5d9f00722245ef641ac65f986e040c3f608a
Алексей Байрышевский <s3mods@yandex.ru>
2023-03-30 14:34:19 +0400

    Added AlertService

---
+++ ui/EquipmentManagement.UI/Abstractions/IAlertService.cs
@@ -0,0 +1,30 @@
+﻿namespace EquipmentManagement.UI.Abstractions;
+
+public enum AlertType
+{
+    Primary,
+    Secondary,
+    Success,
+    Danger,
+    Warning,
+    Info,
+    Light,
+    Dark
+}
+
+public class AlertParameters
+{
+    public AlertType Type { get; }
+    public string Message { get; }
+    public AlertParameters(AlertType type, string message)
+    {
+        Type = type;
+        Message = message;
+    }
+}
+
+public interface IAlertService
+{
+    event EventHandler<AlertParameters> AlertRequested;
+    void Show(AlertType type, string message);
+}

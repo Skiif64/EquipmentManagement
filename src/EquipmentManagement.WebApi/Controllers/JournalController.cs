ae148b10ace6e2670b392b456db8c5fc9313a91c
Алексей Байрышевский <s3mods@yandex.ru>
2023-03-30 16:17:02 +0400

    Added JournalController

---
+++ src/EquipmentManagement.WebApi/Controllers/JournalController.cs
@@ -0,0 +1,42 @@
+﻿using AutoMapper;
+using EquimentManagement.Contracts.Responses;
+using EquipmentManagement.Application.Journaling.GetAll;
+using EquipmentManagement.Application.Journaling.GetWithPaging;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EquipmentManagement.WebApi.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class JournalController : ControllerBase
+{
+    private readonly ISender _sender;
+    private readonly IMapper _mapper;
+
+    public JournalController(ISender sender, IMapper mapper)
+    {
+        _sender = sender;
+        _mapper = mapper;
+    }
+
+    [HttpGet("/all")]
+    public async Task<ActionResult<IEnumerable<JournalRecordResponse>>> GetAllAsync(CancellationToken cancellationToken)
+    {
+        var query = new GetAllJournalQuery();
+        var result = await _sender.Send(query, cancellationToken);
+
+        var response = _mapper.Map<IEnumerable<JournalRecordResponse>>(result);
+        return Ok(response);
+    }
+
+    [HttpGet("/{count:int}/{offset:int}")]
+    public async Task<ActionResult<IEnumerable<JournalRecordResponse>>> GetAsync(int count, int offset, CancellationToken cancellationToken)
+    {
+        var query = new GetLastWithPagingJournalQuery(count, offset);
+        var result = await _sender.Send(query, cancellationToken);
+
+        var response = _mapper.Map<IEnumerable<JournalRecordResponse>>(result);
+        return Ok(response);
+    }
+}

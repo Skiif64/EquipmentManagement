e9a63a76625793acb2cc50a47903368935e2805f
Алексей Байрышевский <s3mods@yandex.ru>
2023-03-30 10:51:06 +0400

    Added ImageModel

---
+++ src/EquipmentManagement.Domain/Models/Image.cs
@@ -0,0 +1,14 @@
+﻿using EquipmentManagement.Domain.Models.Base;
+
+namespace EquipmentManagement.Domain.Models;
+public class Image : BaseModel
+{
+    public Guid EquipmentId { get; init; }
+    public Equipment Equipment { get; init; } = null!;
+    public string FullImagePath { get; init; } = string.Empty;
+    public string ThumbImagePath { get; init; } = string.Empty;
+    protected Image() : base()
+    {
+        
+    }
+}

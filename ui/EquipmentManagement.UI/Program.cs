484e6a178cc2850ad19206d2ae6c9e36b5858a01
Алексей Байрышевский <s3mods@yandex.ru>
2023-03-30 14:57:41 +0400

    Removed alert

--- ui/EquipmentManagement.UI/Program.cs
+++ ui/EquipmentManagement.UI/Program.cs
@@ -43,7 +43,6 @@ builder.Services.AddMemoryCache();
 builder.Services.AddBlazoredLocalStorage();
 builder.Services.AddBlazoredModal();
 builder.Services.AddScoped<ITokenStorage, TokenStorage>();
-builder.Services.AddSingleton<IAlertService, AlertService>();
 var app = builder.Build();
 
 await app.RunAsync();

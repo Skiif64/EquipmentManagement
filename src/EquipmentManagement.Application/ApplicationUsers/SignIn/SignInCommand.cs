be007d72bc9500fb1a064e637d1dff0d1d19281d
Алексей Байрышевский <s3mods@yandex.ru>
2023-03-30 11:12:14 +0400

    Added SiginCommand&handler

---
+++ src/EquipmentManagement.Application/ApplicationUsers/SignIn/SignInCommand.cs
@@ -0,0 +1,9 @@
+﻿using EquipmentManagement.Application.Abstractions;
+using EquipmentManagement.Application.Models;
+
+namespace EquipmentManagement.Application.ApplicationUsers.SignIn;
+public class SignInCommand : ICommand<AuthenticationResult>
+{
+    public string Login { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+}
---
+++ src/EquipmentManagement.Application/ApplicationUsers/SignIn/SignInCommandHandler.cs
@@ -0,0 +1,40 @@
+﻿using EquipmentManagement.Application.Abstractions;
+using EquipmentManagement.Application.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EquipmentManagement.Application.ApplicationUsers.SignIn;
+internal class SignInCommandHandler : ICommandHandler<SignInCommand, AuthenticationResult>
+{
+    private readonly IUserDbContext _context;
+    private readonly IJwtTokenProvider _tokenProvider;
+
+    public SignInCommandHandler(IUserDbContext context, IJwtTokenProvider tokenProvider)
+    {
+        _context = context;
+        _tokenProvider = tokenProvider;
+    }
+
+    public async Task<AuthenticationResult> Handle(SignInCommand request, CancellationToken cancellationToken)
+    {
+        var user = await _context
+            .Set<ApplicationUser>()
+            .SingleOrDefaultAsync(x => x.Login == request.Login, cancellationToken);
+        if (user is null)
+            return AuthenticationResult.CreateFailure(new[]
+            { KeyValuePair.Create("NotFound", "Invalid login.") });
+        if (!user.PasswordEquals(request.Password))
+            return AuthenticationResult.CreateFailure(new[]
+            { KeyValuePair.Create("InvalidPassword", "Invalid password.") });
+
+        var token = _tokenProvider.Generate(user);
+
+        var newRefreshToken = Guid.NewGuid();
+        user.RefreshToken = newRefreshToken;
+        _context
+            .Set<ApplicationUser>()
+            .Update(user);
+        await _context.SaveChangesAsync(cancellationToken);
+
+        return AuthenticationResult.CreateSuccess(token, newRefreshToken.ToString());
+    }
+}

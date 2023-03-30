d0cea47e2daab596eb32d1cc757758aafbf70b29
Алексей Байрышевский <s3mods@yandex.ru>
2023-03-30 15:50:36 +0400

    Added JournalRecordResponse

---
+++ src/EquimentManagement.Contracts/Responses/JournalRecordResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace EquimentManagement.Contracts.Responses;
+public class JournalRecordResponse
+{
+    public Guid? UserId { get; set; }
+    public string? Username { get; set; }
+    public string Message { get; set; } = string.Empty;
+    public string EventName { get; set; } = string.Empty;
+}

65d6e8698b1173fb0e8777f23462effdc63c7c90
Алексей Байрышевский <s3mods@yandex.ru>
2023-04-06 10:18:10 +0400

    Added IFactoryStorageImplementation

---
+++ ui/EquipmentManagement.UI/Authentification/SessionStorageFactory.cs
@@ -0,0 +1,16 @@
+﻿using EquipmentManagement.UI.Abstractions;
+
+namespace EquipmentManagement.UI.Authentification;
+
+public class SessionStorageFactory : ITokenStorageFactory
+{
+    private readonly IServiceProvider _serviceProvider;
+
+    public SessionStorageFactory(IServiceProvider serviceProvider)
+    {
+        _serviceProvider = serviceProvider;
+    }
+
+    public ITokenStorage CreateTokenStorage()
+        => _serviceProvider.GetRequiredService<SessionTokenStorage>();
+}

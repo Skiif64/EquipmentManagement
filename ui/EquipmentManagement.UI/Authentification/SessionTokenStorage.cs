94ed88a433d5cb1534b23794f40b6385a4db0188
Алексей Байрышевский <s3mods@yandex.ru>
2023-04-06 10:17:04 +0400

    Added SessionTokenStorage

---
+++ ui/EquipmentManagement.UI/Authentification/SessionTokenStorage.cs
@@ -0,0 +1,36 @@
+﻿using Blazored.LocalStorage;
+using Blazored.SessionStorage;
+using EquipmentManagement.UI.Abstractions;
+
+namespace EquipmentManagement.UI.Authentification;
+
+public class SessionTokenStorage : ITokenStorage
+{
+    private const string AccessToken = "JwtAccess";
+    private const string RefreshToken = "JwtRefresh";
+    private readonly ISessionStorageService _storage;
+
+    public SessionTokenStorage(ISessionStorageService storage)
+    {
+        _storage = storage;
+    }
+
+    public async Task<string?> GetAccessTokenAsync(CancellationToken cancellationToken = default)
+    => await _storage.GetItemAsStringAsync(AccessToken, cancellationToken);
+
+    public async Task<string?> GetRefreshTokenAsync(CancellationToken cancellationToken = default)
+    => await _storage.GetItemAsStringAsync(RefreshToken, cancellationToken);
+
+    public async Task RemoveAccessTokenAsync(CancellationToken cancellationToken)
+    => await _storage.RemoveItemAsync(AccessToken, cancellationToken);
+
+    public async Task RemoveRefreshTokenAsync(CancellationToken cancellationToken)
+        => await _storage.RemoveItemAsync(RefreshToken, cancellationToken);
+
+    public async Task SetAccessTokenAsync(string token, CancellationToken cancellationToken = default)
+        => await _storage.SetItemAsStringAsync(AccessToken, token, cancellationToken);
+
+
+    public async Task SetRefreshTokenAsync(string token, CancellationToken cancellationToken = default)
+        => await _storage.SetItemAsStringAsync(RefreshToken, token, cancellationToken);
+}

802a6ed01b8cfaa1d414bd2b22fea1b576801191
Алексей Байрышевский <s3mods@yandex.ru>
2023-04-06 10:39:26 +0400

    Added StateStorage

---
+++ ui/EquipmentManagement.UI/States/StateStorage.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Concurrent;
+
+namespace EquipmentManagement.UI.States;
+
+public class StateStorage
+{
+    private readonly ConcurrentDictionary<Type, State> _states = new();
+    public void SetState<T>(Type type, T state) where T : State
+    {       
+        _states.AddOrUpdate(type, state, (key, oldValue) => oldValue = state);
+    }
+
+    public T GetState<T>() where T : State
+    {
+        var stateType = typeof(T);
+        if (!_states.TryGetValue(stateType, out var value))
+            throw new ArgumentException($"State with type {stateType.Name} not containing.");
+
+        return (T)value;
+    }
+}

5e63671e4e4421ba4aa69f32149cb19d7c6e4f27
Алексей Байрышевский <s3mods@yandex.ru>
2023-04-11 13:15:26 +0400

    Added migration

---
+++ src/EquipmentManagement.DAL/Migrations/20230411091358_AddedAuthorToEquipment.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EquipmentManagement.DAL.Migrations
+{
+    public partial class AddedAuthorToEquipment : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Author",
+                table: "Equipments",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "CreatedAt",
+                table: "Equipments",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTimeOffset(new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Author",
+                table: "Equipments");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Equipments");
+        }
+    }
+}

9f1a6cd65ad3ef10dadc48c74f4e77cf06f5fd98
Алексей Байрышевский <s3mods@yandex.ru>
2023-04-11 14:15:23 +0400

    Added Migration

---
+++ src/EquipmentManagement.DAL/Migrations/20230411101342_AddedEmployeeStatus.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EquipmentManagement.DAL.Migrations
+{
+    public partial class AddedEmployeeStatus : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Status",
+                table: "Employees",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "Employees");
+        }
+    }
+}

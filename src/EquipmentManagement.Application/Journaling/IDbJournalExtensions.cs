9ef1bd8e383754d2a287d3a413577dc987953d51
Алексей Байрышевский <s3mods@yandex.ru>
2023-04-14 12:11:47 +0400

    Added DbJournalExtensions

---
+++ src/EquipmentManagement.Application/Journaling/IDbJournalExtensions.cs
@@ -0,0 +1,16 @@
+﻿using EquipmentManagement.Application.Abstractions;
+using Microsoft.Extensions.Logging;
+
+namespace EquipmentManagement.Application.Journaling;
+public static class IDbJournalExtensions
+{
+    public static async Task WriteAsync(this IJournal journal, EventId eventId, string message, CancellationToken cancellationToken = default)
+    {
+        await journal.WriteAsync(eventId, message, null, DateTimeOffset.UtcNow, cancellationToken);
+    }
+
+    public static async Task WriteAsync(this IJournal journal, EventId eventId, string message, string? user, CancellationToken cancellationToken = default)
+    {
+        await journal.WriteAsync(eventId, message, user, DateTimeOffset.UtcNow, cancellationToken);
+    }
+}

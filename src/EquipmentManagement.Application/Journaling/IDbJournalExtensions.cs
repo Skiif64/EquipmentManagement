ad4aeb48a76769302130bb7e756dbb2ee30ce126
Алексей Байрышевский <s3mods@yandex.ru>
2023-04-14 12:12:57 +0400

    Added new WriteAsync method

--- src/EquipmentManagement.Application/Journaling/IDbJournalExtensions.cs
+++ src/EquipmentManagement.Application/Journaling/IDbJournalExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using EquipmentManagement.Application.Abstractions;
 using Microsoft.Extensions.Logging;
+using System.Security.Claims;
 
 namespace EquipmentManagement.Application.Journaling;
 public static class IDbJournalExtensions
@@ -12,5 +13,11 @@ public static class IDbJournalExtensions
     public static async Task WriteAsync(this IJournal journal, EventId eventId, string message, string? user, CancellationToken cancellationToken = default)
     {
         await journal.WriteAsync(eventId, message, user, DateTimeOffset.UtcNow, cancellationToken);
+    }
+
+    public static async Task WriteAsync(this IJournal journal, EventId eventId, string message, ClaimsPrincipal? user, CancellationToken cancellationToken = default)
+    {
+        var username = user?.Identity?.Name;
+        await journal.WriteAsync(eventId, message, username, DateTimeOffset.UtcNow, cancellationToken);
     }
 }

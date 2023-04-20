cbf80fcbb1d87c679479be1d1ab7909b0d986433
Алексей Байрышевский <s3mods@yandex.ru>
2023-04-20 11:20:07 +0400

    Added tokens removal in RedirectHandler

--- ui/EquipmentManagement.UI/Authentification/RedirectToLoginHttpMessageHandler.cs
+++ ui/EquipmentManagement.UI/Authentification/RedirectToLoginHttpMessageHandler.cs
@@ -1,5 +1,7 @@
-﻿using EquipmentManagement.UI.Utils;
+﻿using EquipmentManagement.UI.Abstractions;
+using EquipmentManagement.UI.Utils;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
 using System.Net;
 
 namespace EquipmentManagement.UI.Authentification;
@@ -7,18 +9,25 @@ namespace EquipmentManagement.UI.Authentification;
 public class RedirectToLoginHttpMessageHandler : DelegatingHandler
 {
     private readonly NavigationManager _navigationManager;
+    private readonly JwtAuthentificationStateProvider _authenticationStateProvider;
+    private readonly ITokenStorage _storage;
 
-    public RedirectToLoginHttpMessageHandler(NavigationManager navigationManager)
+    public RedirectToLoginHttpMessageHandler(NavigationManager navigationManager, ITokenStorage storage, JwtAuthentificationStateProvider authenticationStateProvider)
     {
         _navigationManager = navigationManager;
+        _storage = storage;
+        _authenticationStateProvider = authenticationStateProvider;
     }
 
     protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
     {
         var response = await base.SendAsync(request, cancellationToken);
-        if (response.StatusCode is HttpStatusCode.Unauthorized or HttpStatusCode.Forbidden)
+        if (response.StatusCode is HttpStatusCode.Unauthorized)
         {
-            _navigationManager.NavigateTo(PagePaths.Auth.Login);            
+            _navigationManager.NavigateTo(PagePaths.Auth.Login);
+            await _storage.RemoveRefreshTokenAsync(cancellationToken);
+            await _storage.RemoveAccessTokenAsync(cancellationToken);
+            await _authenticationStateProvider.LogoutUser();
         }
         return response;        
     }

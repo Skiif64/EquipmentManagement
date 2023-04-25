599dd7394544e8f57075fbc9bcf5c76014c1eada
Алексей Байрышевский <s3mods@yandex.ru>
2023-04-25 16:02:26 +0400

    Added RemoveImagesCommand&handler

---
+++ src/EquipmentManagement.Application/Images/RemoveByNames/RemoveImagesCommandHandler.cs
@@ -0,0 +1,29 @@
+﻿using EquipmentManagement.Application.Abstractions;
+using EquipmentManagement.Domain.Models;
+
+namespace EquipmentManagement.Application.Images.RemoveByNames;
+internal class RemoveImagesCommandHandler : ICommandHandler<RemoveImagesCommand>
+{
+    private readonly IApplicationDbContext _context;
+    private readonly IImageStorage _imageStorage;
+    public RemoveImagesCommandHandler(IApplicationDbContext context, IImageStorage imageStorage)
+    {
+        _context = context;
+        _imageStorage = imageStorage;
+    }
+    public async Task Handle(RemoveImagesCommand request, CancellationToken cancellationToken)
+    {
+        var images = _context
+            .Set<Image>()
+            .Where(x => x.EquipmentId == request.EquipmentId)
+            .AsEnumerable()
+            .Where(x => request.ImageNames.Any(n => n == x.FullImagePath));
+
+        _context
+            .Set<Image>()
+            .RemoveRange(images);
+
+        await _imageStorage.RemoveImagesAsync(request.ImageNames, cancellationToken);
+        await _context.SaveChangesAsync(cancellationToken);
+    }
+}

acd5eeabf566d387cfcedb53fb7f55cf6b3b7858
Алексей Байрышевский <s3mods@yandex.ru>
2023-04-26 10:21:38 +0400

    Russified AppLogEvents

--- src/EquipmentManagement.Application/AppLogEvents.cs
+++ src/EquipmentManagement.Application/AppLogEvents.cs
@@ -4,11 +4,11 @@ namespace EquipmentManagement.Application;
 
 public class AppLogEvents
 {
-    public static readonly EventId Create = new(1000, "Create");
-    public static readonly EventId Read = new(1001, "Read");
-    public static readonly EventId Update = new(1002, "Update");
-    public static readonly EventId Delete = new(1003, "Delete");
+    public static readonly EventId Create = new(1000, "Создано");
+    public static readonly EventId Read = new(1001, "Считано");
+    public static readonly EventId Update = new(1002, "Обновлено");
+    public static readonly EventId Delete = new(1003, "Удалено");
 
-    public static readonly EventId Login = new(2001, "Login");
-    public static readonly EventId Register = new(2002, "Register");
+    public static readonly EventId Login = new(2001, "Логин");
+    public static readonly EventId Register = new(2002, "Регистрация");
 }

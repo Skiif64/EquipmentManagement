41a173337757b56d589a5eeb6530218b682d50b9
Алексей Байрышевский <s3mods@yandex.ru>
2023-08-01 10:16:15 +0400

    Start cleaning equipment logic

---
+++ src/EquipmentManagement.Application/Equipments/Get/GetAllEquipmentWithStatusQuery.cs
@@ -0,0 +1,18 @@
+﻿using EquipmentManagement.Application.Abstractions;
+using EquipmentManagement.Domain.Models;
+
+namespace EquipmentManagement.Application.Equipments.Get;
+
+public class GetAllEquipmentWithStatusQuery : IQuery<PagedList<Equipment>>
+{
+    public int? Page { get; }
+    public int? PageSize { get; }
+    public string? Order { get; }
+
+    public GetAllEquipmentWithStatusQuery(int? page, int? pageSize, string? order)
+    {
+        Page = page;
+        PageSize = pageSize;
+        Order = order;
+    }
+}
---
+++ src/EquipmentManagement.Application/Equipments/Get/GetAllEquipmentWithStatusQueryHandler.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using EquipmentManagement.Application.Abstractions;
+using EquipmentManagement.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EquipmentManagement.Application.Equipments.Get;
+
+public class GetAllEquipmentWithStatusQueryHandler
+    : IQueryHandler<GetAllEquipmentWithStatusQuery, PagedList<Equipment>>
+{
+    private const int Page = 1;
+    private const int PageSize = 10;
+    private const string Order = "ASC";
+    private readonly IApplicationDbContext _context;
+    private readonly IMapper _mapper;
+
+    public GetAllEquipmentWithStatusQueryHandler(IApplicationDbContext context, IMapper mapper)
+    {
+        _context = context;
+        _mapper = mapper;
+    }
+
+    public async Task<PagedList<Equipment>> Handle(GetAllEquipmentWithStatusQuery request,
+        CancellationToken cancellationToken)
+    {
+        var entities = _context
+            .Set<Equipment>()
+            .Include(x => x.Images)
+            .Include(x => x.Type);
+
+        int page = request.Page ?? Page;
+        int pageSize = request.PageSize ?? PageSize;
+        return await PagedList<Equipment>.CreateAsync(entities, page, pageSize);
+    }
+}

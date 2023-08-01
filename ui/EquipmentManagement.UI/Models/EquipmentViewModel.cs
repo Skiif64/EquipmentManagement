960c93cb5943a4c289836096e937868c5f50d952
Алексей Байрышевский <s3mods@yandex.ru>
2023-08-01 11:25:42 +0400

    Added EquipmentViewModel

---
+++ ui/EquipmentManagement.UI/Models/EquipmentViewModel.cs
@@ -0,0 +1,9 @@
+﻿using EquimentManagement.Contracts.Responses;
+
+namespace EquipmentManagement.UI.Models;
+
+public class EquipmentViewModel : EquipmentResponse
+{
+    public string? CurrentStatusTitle { get; init; }
+    public string? EmployeeFullname { get; init; }
+}

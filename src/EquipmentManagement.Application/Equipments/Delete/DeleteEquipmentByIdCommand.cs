9c91cc3ddcb15dd674d1de87f6d61aa9f3460960
Алексей Байрышевский <s3mods@yandex.ru>
2023-08-11 13:54:55 +0400

    Added DeleteEquipmentCommand & Handler

---
+++ src/EquipmentManagement.Application/Equipments/Delete/DeleteEquipmentByIdCommand.cs
@@ -0,0 +1,11 @@
+﻿using EquipmentManagement.Application.Abstractions;
+
+namespace EquipmentManagement.Application.Equipments.Delete;
+public class DeleteEquipmentByIdCommand : ICommand
+{
+    public Guid Id { get; }
+    public DeleteEquipmentByIdCommand(Guid id)
+    {
+        Id = id;
+    }
+}
---
+++ src/EquipmentManagement.Application/Equipments/Delete/DeleteEquipmentByIdCommandHandler.cs
@@ -0,0 +1,35 @@
+﻿using EquipmentManagement.Application.Abstractions;
+using EquipmentManagement.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EquipmentManagement.Application.Equipments.Delete;
+internal class DeleteEquipmentByIdCommandHandler : ICommandHandler<DeleteEquipmentByIdCommand>
+{
+    private readonly IApplicationDbContext _context;
+
+    public DeleteEquipmentByIdCommandHandler(IApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task Handle(DeleteEquipmentByIdCommand request, CancellationToken cancellationToken)
+    {
+        var recordsToDelete = _context
+            .Set<EquipmentRecord>()
+            .Where(x => x.EquipmentId == request.Id);
+
+        _context
+            .Set<EquipmentRecord>()
+            .RemoveRange(recordsToDelete);
+
+        var equipmentToDelete = await _context
+            .Set<Equipment>()
+            .SingleAsync(x => x.Id == request.Id);
+
+        _context
+            .Set<Equipment>()
+            .Remove(equipmentToDelete);
+
+        await _context.SaveChangesAsync(cancellationToken);
+    }
+}

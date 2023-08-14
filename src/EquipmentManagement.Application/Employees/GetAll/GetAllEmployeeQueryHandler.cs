19a3d2bccc85b073f75eb331388748bc392d8643
Алексей Байрышевский <s3mods@yandex.ru>
2023-08-14 10:16:23 +0400

    Returned GetAllEmployeeQuery & handler

---
+++ src/EquipmentManagement.Application/Employees/GetAll/GetAllEmployeeQueryHandler.cs
@@ -0,0 +1,24 @@
+﻿using EquipmentManagement.Application.Abstractions;
+using EquipmentManagement.Domain.Models;
+
+namespace EquipmentManagement.Application.Employees.GetAll;
+internal sealed class GetAllEmployeeQueryHandler : IQueryHandler<GetAllEmployeeQuery, IEnumerable<Employee>>
+{
+    private readonly IApplicationDbContext _context;
+
+    public GetAllEmployeeQueryHandler(IApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public Task<IEnumerable<Employee>> Handle(GetAllEmployeeQuery request, CancellationToken cancellationToken)
+    {
+        var set = _context
+            .Set<Employee>()
+            .OrderBy(x => x.Status)
+            .ThenBy(x => x.Lastname)
+            .ThenBy(x => x.Firstname)
+            ;
+        return Task.FromResult(set.AsEnumerable());
+    }
+}

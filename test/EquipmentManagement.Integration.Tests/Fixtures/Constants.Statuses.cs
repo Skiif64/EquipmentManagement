4b97306c7217a94ef832988c794835fc0f01d3eb
Алексей Байрышевский <s3mods@yandex.ru>
2023-08-29 20:02:13 +0400

    Added test constants

---
+++ test/EquipmentManagement.Integration.Tests/Fixtures/Constants.Statuses.cs
@@ -0,0 +1,28 @@
+﻿using EquipmentManagement.Domain.Models;
+
+namespace EquipmentManagement.Integration.Tests.Fixtures;
+public static partial class Constants
+{
+    public static class Statuses
+    {
+        public static readonly Guid StatusAId = Guid.NewGuid();
+        public static readonly Guid StatusBId = Guid.NewGuid();
+
+        public const string StatusAName = "Status A";
+        public const string StatusBName = "Status B";
+
+        public static Status CreateA()
+            => new Status
+            {
+                Id = StatusAId,
+                Title = StatusAName,
+            };
+
+        public static Status CreateB()
+            => new Status
+            {
+                Id = StatusBId,
+                Title = StatusAName,
+            };
+    }
+}
